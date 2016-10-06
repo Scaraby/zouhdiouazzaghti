03b013b3261f0e43fe31a234a5f55912b342b8d9
zakaria.zouhdi@ensea.fr <zakaria.zouhdi@ensea.fr>
2016-10-06 15:11:20 +0200

    Wall.cpp

---
+++ src/Wall.cpp
@@ -0,0 +1,39 @@
+class Wall : public StaticElement {
+  // Operations
+public:
+  Wall ( id:WallTypeId)
+	{
+
+	}
+
+
+  bool const isSpace ();
+	{
+
+	}
+
+  TypeId const getTypeId ();
+	{
+
+	}
+
+  Element* const clone ();
+	{
+
+	}
+
+  bool const equals (const Element& other);
+	{
+
+	}
+
+  WallTypeId const getWallTypeId ();
+	{
+
+	}
+
+  void setWallTypeId (WallTypeId id);
+	{
+
+	}
+};
