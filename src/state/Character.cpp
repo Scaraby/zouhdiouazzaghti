feeb9c4abbda0c0f394984331872e2c4d116b7aa
Reda OUAZZAGHTI <reda951@gmail.com>
2016-10-12 10:06:08 +0200

    Character.cpp

--- src/state/Character.cpp
+++ src/state/Character.cpp
@@ -2,9 +2,44 @@
 
 namespace state{
 
- Character::Character() 
- {
+ Character::Character(){
     
  }
 
+ bool const Character::isCharacter(){
+     
+ }
+ 
+ int Character::getSpeed(){
+     
+ }
+ 
+ int Character::getStrength(){
+     
+ }
+ 
+ int Character::getXP(){
+     
+ }
+ 
+ void Character::setSpeed(int speed){
+     
+ }
+ 
+ void Character::setStrength(int strength){
+     
+ }
+ 
+ void Character::setXP(int xp){
+     
+ }
+ 
+ void Character::attack(){
+     
+ }
+ 
+ Character::~Character(){
+     
+ }
+ 
 }
