12b0e24395cef55105c482b47881d2901cdfef67
Reda OUAZZAGHTI <reda951@gmail.com>
2016-10-17 16:43:43 +0200

    Rendu

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,49 @@
 //coucou lotestl
 //Salut  Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
+#include "Rendu.hpp"
 
-void testSFML() {
+int testSFML() {
     sf::Texture texture;
+    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+
+    // on définit le niveau à l'aide de numéro de tuiles
+     int level[] =
+    {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+    
+     
+    
+    // on crée la tilemap avec le niveau précédemment défini
+    TileMap map;
+    if (!map.load("../Textures/PNG/mapTile_001.png", sf::Vector2u(32, 32), level, 16, 8))
+        return -1;
+
+    // on fait tourner la boucle principale
+    while (window.isOpen())
+    {
+        // on gère les évènements
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // on dessine le niveau
+        window.clear();
+        window.draw(map);
+        window.display();
+    }
+    return 0;
 }
 
 // Fin test SFML
@@ -19,8 +59,9 @@ using namespace std;
 
 int main(int argc,char* argv[]) 
 {
-
+ 
     cout << "It works !" << endl;
+    testSFML();
 
     return 0;
 }
