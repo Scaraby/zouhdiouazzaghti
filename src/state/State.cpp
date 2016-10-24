fb80c5145397350c5479c5aeb896fab97e9a40af
zakaria.zouhdi@ensea.fr <zakaria.zouhdi@ensea.fr>
2016-10-24 16:08:12 +0200

    statemap

--- src/state/State.cpp
+++ src/state/State.cpp
@@ -1,22 +1,54 @@
 #include "State.h"
-
+#include <iostream> 
+#include <stdlib.h> 
+#include <vector>
+#include <fstream>
 using namespace std;
 
 namespace state {
 State::State(){
-    const int level0[] =
-    {
-        10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-        10, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 10,
-        10, 3, 4, 9, 4, 4, 4, 4, 4, 4, 4, 4, 9, 4, 5, 10,
-        10, 3, 4, 4, 4, 9, 4, 4, 4, 4, 4, 4, 4, 4, 5, 10,
-        10, 3, 9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 10,
-        10, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 9, 4, 4, 5, 10,
-        10, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 10,
-        10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
-    };
-     level.resize(128);
-     for(size_t i=0;i<level.size();i++) level[i] = level0[i];
+{      
+  int array_size = 128; 
+	 vector<char> array(array_size);
+	int position = 0; 
+
+	ifstream fin("test.txt"); 
+  if(fin.is_open())
+	{
+
+    cout << "File Opened successfully!!!. Reading data from file into array" << endl;
+
+		while(!fin.eof() && position < array_size)
+		{
+			fin.get(array[position]); 
+			position++;
+		}
+		array[position-1] = '\0'; 
+    
+    cout << "Displaying Array..." << endl << endl;
+    //this loop display all the charaters in array till \0 
+		for(int i = 0; array[i] != '\0'; i++)
+		{
+			cout << array[i] << endl;
+		}
+		
+		for(int i = 0; array[i] != '\0'; i++)
+		{
+			level[i]=array[i]-97 ;
+		}
+	
+	    cout << "Displaying Level..." << endl << endl;
+    //this loop display all the charaters in array till \0 
+		for(int i = 0; i <array_size-1; i++)
+		{
+			cout << level[i] << endl;
+		}
+	
+	}
+	else 
+		cout << "File could not be opened." << endl;
+	}
+	
    
 }
 
