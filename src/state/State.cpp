9da198fa6c05336d3896799b02a063f65e0f0589
Reda OUAZZAGHTI <reda951@gmail.com>
2016-10-25 12:00:41 +0200

    rendu

--- src/state/State.cpp
+++ src/state/State.cpp
@@ -8,46 +8,43 @@ using namespace std;
 namespace state {
 State::State(){
 {      
-  int array_size = 128; 
-	 vector<char> array(array_size);
-	int position = 0; 
-    level.resize(128);
-	ifstream fin("src/test.txt"); 
-  if(fin.is_open())
-	{
+  int array_size = 128;
+  vector<char> array(array_size);
+  int position = 0; 
+  level.resize(128);
+  ifstream fin("src/test.txt"); 
+  if(fin.is_open()){
 
     cout << "File Opened successfully!!!. Reading data from file into array" << endl;
-
-		while(!fin.eof() && position < array_size)
-		{
-			fin.get(array[position]); 
-			position++;
-		}
-		array[position-1] = '\0'; 
+    while(!fin.eof() && position < array_size)	{
+        fin.get(array[position]); 
+	position++;
+	}
+   
     
     cout << "Displaying Array..." << endl << endl;
     //this loop display all the charaters in array till \0 
-		for(int i = 0; array[i] != '\0'; i++)
-		{
-			cout << array[i] << endl;
-		}
+    for(int i = 0; i<array.size(); i++)
+	{
+	cout << array[i] << endl;
+	}
 		
-		for(int i = 0; array[i] != '\0'; i++)
-		{
-			level[i]=array[i]-97 ;
-		}
+    for(int i = 0; i<array.size(); i++)
+	{
+	level[i]=array[i]-97 ;
+	}
 	
-	    cout << "Displaying Level..." << endl << endl;
+     cout << "Displaying Level..." << endl << endl;
     //this loop display all the charaters in array till \0 
-		for(int i = 0; i <array_size-1; i++)
-		{
-			cout << level[i] << endl;
-		}
+    for(int i = 0; i <array_size; i++)
+    {
+     cout << level[i] << endl;
+    }
 	
-	}
-	else 
-		cout << "File could not be opened." << endl;
-	}
+}
+	
+  else cout << "File could not be opened." << endl;
+    }
 	
    
 }
