b04c1fb6a5195bb21b43fee586977901ae172aa7
zakaria.zouhdi@ensea.fr <zakaria.zouhdi@ensea.fr>
2016-11-23 09:31:44 +0100

    IA moy

--- src/ia/IA.cpp
+++ src/ia/IA.cpp
@@ -25,22 +25,26 @@ Direction IA::ExecuteGroupAI (Engine moteur, vector<Character*> group){
     int x = rand() % 4;
     dir = (Direction)x;
     	if ( group[0]->getY() < group[1]->getY() + 75)
-			{moteur.moveElement(Down, group[0]);}
-		
+			{moteur.moveElement(Down, group[0]);
+                        moteur.moveElement(dir, group[1]);}
+             
 		
 
 		if ( group[0]->getY() > group[1]->getY() - 75)
-			{moteur.moveElement(Up, group[0]);}
+			{moteur.moveElement(Up, group[0]);
+                        moteur.moveElement(dir, group[1]);}
 		
 		
 
 		if (group[0]->getX() < group[1]->getX() + 50)
-			{moteur.moveElement(Right, group[0]);}
+			{moteur.moveElement(Right, group[0]);
+                        moteur.moveElement(dir, group[1]);}
 		
 		
 
 		if (group[0]->getX() > group[1]->getX() - 50)
-			{moteur.moveElement(Left, group[0]);}
+			{moteur.moveElement(Left, group[0]);
+                        moteur.moveElement(dir, group[1]);}
 		
     return dir;
 
