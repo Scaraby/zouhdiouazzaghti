ec508e836b1d3bed8941e72034aa4ec3f3598b3a
Reda OUAZZAGHTI <reda951@gmail.com>
2016-11-28 16:06:52 +0100

    attack2

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -11,30 +11,32 @@ using namespace std;
 
 
     void Engine::moveElement(state::Direction dir, state::Character* character) {  //Le principe ici est d'envoyer un message au state pour modifier les attributs x, y et DIRECTION (attribut à rajouter) de ELEMENT. Il faudra que la position du sprite dépende directement de,int a=Element.x et int b=Element.y et de la direction (les int a et int b c'est des exemples d'instanciation pour éviter de faire un gros bloc illisible quand t'appelle sprite.move(a , b). Voilà, si t'arrives à faire ça on aura au moins fait ça pour la partie moteur... Restera plus que le RESTE mdr.
-if (!(character->getTurn()))
-{std::cout << "Wait for your turn\n";}
-else{  switch (dir){
-        case Right:
-            character->setX(character->getX()+52);
-            break;
+        if (!(character->getTurn())) std::cout << "Wait for your turn" << endl;
+
+        else
+        {  
+         switch (dir){
+             case Right:
+               character->setX(character->getX()+52);
+                break;
             
-        case Left :
-            character->setX(character->getX()-52);
-            break;
+             case Left :
+                 character->setX(character->getX()-52);
+                 break;
            
             
-        case Up:
-            character->setY(character->getY()-52);
-            break;
+             case Up:
+                 character->setY(character->getY()-52);
+                 break;
             
-        case Down:
-            character->setY(character->getY()+52);
-            break;
+             case Down:
+                character->setY(character->getY()+52);
+               break;
             
-        default:
-            break;
+            default:
+               break;
             
-        }
+         }
 
     }
 }
