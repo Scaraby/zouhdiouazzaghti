d40d76d455fbf049d5fde4a86b30d36333524d06
zakaria.zouhdi@ensea.fr <zakaria.zouhdi@ensea.fr>
2016-12-14 14:48:25 +0100

    lol

--- src/server.h
+++ src/server.h
@@ -2,5 +2,9 @@
 #ifndef __SERVER__H
 #define __SERVER__H
 
-#include "server/AbstractServices.h"
+#include "server/HttpStatus.h"
+#include "server/AbstractService.h"
+#include "server/UserService.h"
+#include "server/ServiceException.h"
+#include "server/ServicesManager.h"
 #endif
